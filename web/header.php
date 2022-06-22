eae4089f35efddea1f653bee84b072b8634b3644
Thiago Quevedo <atendimento@alavanca.digital>
2022-06-21 23:29:17 -0300

    Header

--- web/header.php
+++ web/header.php
@@ -7,19 +7,19 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 
-<meta property="og:title" content="titulo" />
+<meta property="og:title" content="Pax Empreendimentos" />
 <meta property="og:description" content="descricao" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="http://www.seusite.com.br" />
+<meta property="og:url" content="http://www.pax.com.br" />
 <meta property="og:image" content="imagem - 1200x630" />
-<meta property="og:site_name" content="titulo" />
+<meta property="og:site_name" content="Pax Empreendimentos" />
 <?php /*?>Pagina para verifica��o: https://developers.facebook.com/tools/debug<?php */?>
-<meta name="Title" content="titulo"/>
-<meta name="reply-to" content="email do cliente"/>
+<meta name="Title" content="Pax Empreendimentos"/>
+<meta name="reply-to" content="paxatendimento@pax.com.br"/>
 <meta name="description" content="descricao"/>
 <meta name="keywords" content=""/>
-<meta name="copyright" content="Copyright 2021"/>
-<meta name="DC.date.created" content="AAAA-MM-DD" />
-<meta name="URL" content="http://www.seusite.com.br" />
+<meta name="copyright" content="Copyright 2022"/>
+<meta name="DC.date.created" content="2022-07-01" />
+<meta name="URL" content="http://www.pax.com.br" />
 <style type="text/css"><?php echo file_get_contents ('css/all.min.css');?></style>
 <?php include("analytics.php"); ?>
