430229985f17b4af6643ad0e4fcd505f806d9362
josi123 <josiane@brsis.com.br>
2022-06-23 11:46:29 -0300

    ajuste banner

--- web/banner_lista.php
+++ web/banner_lista.php
@@ -40,7 +40,50 @@
 
         </div>
       </div>
-        <?php /*?><img src="img/elemento-banner.png" class="efeito" alt="PAX Empreendimentos"><?php */?>
+      <img src="img/elemento-banner.png" class="efeito" alt="PAX Empreendimentos">
+  </div>
+  <div>
+    <picture>
+      <source media="(max-width: 1024px)" srcset="img/banner-mob.jpg">
+      <source media="(min-width: 1800px)" srcset="img/banner-v1.png">
+      <img src="img/banner-v1.png" class="img-responsive center-block" alt="Conquiste o conforto e modernidade sem abrir mão do luxo">
+    </picture>
+      <h2 class="frase">
+        <a href="empreendimento">
+          <strong>
+            Conquiste o conforto
+            e modernidade sem
+            abrir mão do luxo
+          </strong>
+          <small>É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página.</small>
+          <span><strong>Descubra mais</strong> </span>
+        </a>
+      </h2>
+
+      <div class="infos">
+        <p>
+          Acqua Residence
+          <small>Em construção </small>
+        </p>
+        <div class="espacos">
+          <span>
+            <img src="img/cama.svg" alt="Suítes">
+            Suíte + 1
+          </span>
+          <span>|</span>
+          <span>
+            <img src="img/home.svg" alt="Metros">
+            72 a 84m²
+          </span>
+          <span> |</span>
+          <span>
+            <img src="img/carro.svg" alt="Vagas">
+            1 vaga
+          </span>
+
+        </div>
+      </div>
+      <img src="img/elemento-banner.png" class="efeito" alt="PAX Empreendimentos">
   </div>
 
 </div>
